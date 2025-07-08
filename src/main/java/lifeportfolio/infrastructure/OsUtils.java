e8e7f076292035b2c3327142446d79baded211c8
Moonlight <moonlight9512@gmail.com>
2025-07-08 17:35:11 +0500

    Set UNIX systems to open directory, not file

--- src/main/java/lifeportfolio/infrastructure/OsUtils.java
+++ src/main/java/lifeportfolio/infrastructure/OsUtils.java
@@ -5,8 +5,16 @@ import java.io.IOException;
 
 import lifeportfolio.enums.OS;
 
+/**
+ * Utilities for working with Operational System.
+ */
 public class OsUtils {
 	
+	/**
+	 * Get the currently used Operational System.
+	 * @return Enum describing the currently used OS
+	 * ({@code OS.UNKNOWN} if could not fetch).
+	 */
 	public static OS getOs() {
 		String osName = System.getProperty("os.name").toLowerCase();
 		if (osName.startsWith("win")) {
@@ -20,6 +28,14 @@ public class OsUtils {
 		}
 	}
 	
+	/**
+	 * For Windows: opens a directory, containing
+	 * the file, in System Explorer, and selects (highlights) it. <br>
+	 * For UNIX: Simply opens the said directory, using appropriate
+	 * File Manager.
+	 * @param fileToOpen File inside a directory that must be opened in Explorer.
+	 * @throws IOException If can't open for some reason.
+	 */
 	public static void openInSystem(File fileToOpen) throws IOException {
 		switch (getOs()) {
 		case WINDOWS:
@@ -28,11 +44,11 @@ public class OsUtils {
 			break;
 		case LINUX:
 			Runtime.getRuntime()
-			.exec(String.format("xdg-open \"%s\"", fileToOpen.getAbsolutePath()));
+			.exec(String.format("xdg-open \"%s\"", fileToOpen.getParentFile()));
 			break;
 		case MACOS:
 			Runtime.getRuntime()
-			.exec(String.format("open \"%s\"", fileToOpen.getAbsolutePath()));
+			.exec(String.format("open \"%s\"", fileToOpen.getParentFile()));
 			break;
 		default:
 			throw new RuntimeException("Could not open file: Unknown Operational System");
